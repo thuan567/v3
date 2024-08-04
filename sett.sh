200635956545b7739a55e012e403afd617773989
thuan567 <96828228+thuan567@users.noreply.github.com>
2024-08-04 16:38:20 +0700

    Delete sett.sh

--- sett.sh
+++
@@ -1,1 +0,0 @@
-clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/thuanht567/v3/raw/main/v3.zip && unzip v2.zip && chmod +x * && t
